6cc47d88a5cdfe9ef08f3632b8c178d51a6d054a
meghanaraokl <meghanaraokl@gmail.com>
2021-01-13 19:45:21 +0530

    commit

---
+++ University_Database/University_Database/dbo/Functions/GetNthQuantity.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION [dbo].[getNthQuantity] (@n INT)
+RETURNS INT
+AS
+BEGIN
+declare	@quantity INT
+
+	SELECT @quantity = quantity
+
+	FROM (
+		SELECT Quantity
+			,DENSE_RANK() OVER (
+				ORDER BY quantity DESC
+				) AS Rankid
+		FROM Tbl_Book
+		) AS Result
+	WHERE Rankid = 1
+
+	RETURN @quantity
+END
